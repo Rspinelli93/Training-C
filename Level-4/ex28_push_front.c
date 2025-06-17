60b3848f3525f28942c97e81de7f6921bbc10332
Rspinelli93 <rspinellir13@gmail.com>
2025-06-17 15:39:24 +0200

    Ex28

--- Level-4/ex28_push_front.c
+++ Level-4/ex28_push_front.c
@@ -6,7 +6,51 @@ Description: Insert a new node at the beginning.
 #include <stdio.h>
 #include <stdlib.h>
 
+typedef struct node
+{
+    int value;
+    struct node *next;
+} node;
+
 int main (void)
 {
+    node *start = NULL;
+
+    for (int i = 0; i < 4; i++)
+    {
+        node *n = malloc(sizeof(node));
+
+        if (n == NULL)
+        {
+            return 1;
+        }
+
+        n -> value = i;
+        n -> next = start;
+        
+        start = n;
+    }
+
+    node *ptr = start;
+
+    while(ptr != NULL)
+    {
+        printf("%i\n", ptr->value);
+        ptr = ptr->next;
+    }
+
+    node *newNode = malloc(sizeof(node));
+    newNode -> value = 100;
+    newNode -> next = start;
+    start = newNode;
+
+    ptr = start;
+
+    while(ptr != NULL)
+    {
+        printf("%i\n", ptr->value);
+        ptr = ptr->next;
+    }
+
     return 0;
 }
