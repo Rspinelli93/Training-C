e4465fce0282b1e27d4998abb28d4e0c72891654
Rspinelli93 <rspinellir13@gmail.com>
2025-06-17 17:01:00 +0200

    ex29

--- Level-4/ex29_push_back.c
+++ Level-4/ex29_push_back.c
@@ -6,7 +6,67 @@ Description: Insert a new node at the end.
 #include <stdio.h>
 #include <stdlib.h>
 
+typedef struct node
+{
+    int value;
+    struct node *next;
+} node;
+
+const int SIZE = 5;
+
 int main (void)
 {
+    node *start = NULL;
+
+    for (int i = 0; i < SIZE; i++)
+    {
+        node *n = malloc(sizeof(node));
+
+        if (n == NULL)
+        {
+            printf("Could not allocate node\n");
+            return 1;
+        }
+
+        n->value = i;
+        n->next = start;
+
+        start = n;
+    }
+
+    node *ptr = start;
+
+    while(ptr != NULL)
+    {
+        printf("%i\n", ptr->value);
+        if(ptr->next == NULL)
+        {
+            node *newNode = malloc(sizeof(node));
+            newNode->value = 100;
+            newNode->next = NULL;
+
+            ptr->next = newNode;
+            break;
+        }
+        ptr = ptr->next;
+    }
+
+    if(ptr->next == NULL)
+    {
+        node *newNode = malloc(sizeof(node));
+        newNode->value = 100;
+        newNode->next = NULL;
+
+        ptr->next = newNode;
+    }
+
+    ptr = start;
+
+    while(ptr != NULL)
+    {
+        printf("%i\n", ptr->value);
+        ptr = ptr->next;
+    }
+
     return 0;
 }
