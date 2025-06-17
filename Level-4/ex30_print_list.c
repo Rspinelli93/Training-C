bb34cf0345e5ca5e0b7028deac0d0e25a9bc9324
Rspinelli93 <rspinellir13@gmail.com>
2025-06-17 17:36:56 +0200

    Ex30

--- Level-4/ex30_print_list.c
+++ Level-4/ex30_print_list.c
@@ -6,7 +6,40 @@ Description: Traverse and print all nodes.
 #include <stdio.h>
 #include <stdlib.h>
 
+typedef struct node
+{
+    int value;
+    struct node *next;
+} node;
+
+const int SIZE = 4;
+
 int main (void)
 {
+    node *start = NULL;
+
+    for (int i = 0; i < SIZE; i++)
+    {
+        node *n = malloc(sizeof(node));
+
+        if(n == NULL)
+        {
+            printf("No memory allocated\n");
+            return 1;
+        }
+
+        n->value = i;
+        n->next = start;
+
+        start = n;
+    }
+
+    node *ptr = start;
+
+    while(ptr != NULL)
+    {
+        printf("%i\n", ptr->value);
+        ptr = ptr->next;
+    }
     return 0;
 }
